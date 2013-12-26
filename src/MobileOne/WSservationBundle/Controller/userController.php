643edb3a67260a897552814f3fb530ea50bca9d2
Bastienp <bastien.poidevain@gmail.com>
2013-12-26 11:25:15 +0100

    Add post Users

--- src/MobileOne/WSservationBundle/Controller/userController.php
+++ src/MobileOne/WSservationBundle/Controller/userController.php
@@ -1,8 +1,10 @@
 <?php
 namespace MobileOne\WSservationBundle\Controller;
-use MobileOne\WSservationBundle\Entity\UserType;
+use MobileOne\WSservationBundle\Entity;
+use MobileOne\WSservationBundle\Entity\User;
 use MobileOne\WSservationBundle\Entity\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
 use JMS\SecurityExtraBundle\Security\Util\String;
 use FOS\RestBundle\View\View;
@@ -10,7 +12,14 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\RestBundle\FOSRestBundle;
-class userController extends controller
+use Symfony\Component\BrowserKit\Request;
+//use Doctrine\Tests\Models\Quote\User;
+//use MobileOne\WSservationBundle\Form\UserType;
+use MobileOne\WSservationBundle\Form;
+use MobileOne\WSservationBundle\Form\UserType;
+
+
+class userController extends Controller
 {
 	/**
 	 * 
@@ -60,11 +69,8 @@ class userController extends controller
 		$repository = $this->container->get('doctrine')
 		->getManager()
 		->getRepository('MobileOneWSservationBundle:User');
-			
-		$user = $repository->findBy(array('email' => 'testEmail' ),
-									array('email' => 'desc'),
-									5,
-									0);
+		
+		$user = $repository->findOneBy(array('email'=>$email));
 		$userPassword = $user->getPassword();
 		if($userPassword == $pass)
 		{
@@ -78,6 +84,34 @@ class userController extends controller
 		
 		
 	}
+	
+
+	
+	public function postUserAction()
+	{
+
+		$request = $this->get('request');
+		$content = json_decode($request->getContent());
+		
+		$user = new User();
+		$user	->setFirstName($content->{'firstName'});
+		$user	->setLastName($content->{'lastName'});
+		$user	->setEmail($content->{'email'});
+		$user	->setPassword($content->{'password'});
+		
+		
+
+		// Persist les données
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($user);
+		$em->flush();
+			
+
+		return $user;
+		
+		
+	}
+	
 
 	
 
